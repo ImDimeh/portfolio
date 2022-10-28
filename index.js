23a948732ada348810e7f9a9739503a3f4e47c1d
MEHDI BELLAM <mehdi.bellam@outlok.fr>
2022-10-28 08:48:48 +0200

    :construction: regler le problemes des social medias dans le menu mobile :construction:

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const menuHamburger = document.querySelector(".menu-burger");
 const navLinks = document.querySelector(".nav-links");
+const social_network = document.querySelector(".social-network >");
 
 menuHamburger.addEventListener("click", () => {
   navLinks.classList.toggle("mobile-menu");
+  social_network.classList.toggle("mobile-social-network");
 });
