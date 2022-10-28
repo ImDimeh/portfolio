f7a6648d619fba34dc4252f5853f301dbe8e10e8
MEHDI BELLAM <mehdi.bellam@outlok.fr>
2022-10-28 09:12:42 +0200

    :construction: le changement de classe fonctionne mais que pour le premier social media :construction:

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const menuHamburger = document.querySelector(".menu-burger");
 const navLinks = document.querySelector(".nav-links");
-const social_network = document.querySelector(".social-network >");
+const social_network = document.querySelector("div.social-network > ul > li  ");
 
 menuHamburger.addEventListener("click", () => {
   navLinks.classList.toggle("mobile-menu");
