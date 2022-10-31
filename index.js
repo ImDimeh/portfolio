487daac8261d0351bddf6cf172c995de03ea3976
MEHDI BELLAM <mehdi.bellam@outlok.fr>
2022-10-31 12:06:30 +0100

    bug résolu ( syntax error ) pour les social network

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const menuHamburger = document.querySelector(".menu-burger");
 const navLinks = document.querySelector(".nav-links");
-const social_network = document.querySelector("div.social-network > ul > li  ");
+const social_network = document.querySelectorAll("div.social-network ul li ");
 
 menuHamburger.addEventListener("click", () => {
   navLinks.classList.toggle("mobile-menu");
-  social_network.classList.toggle("mobile-social-network");
 });
+
+menuHamburger.addEventListener("click", () => {
+  social_network.forEach((social_network) => {
+    social_network.classList.toggle("mobile-social-network");
+  });
+});
+console.log(social_network);
