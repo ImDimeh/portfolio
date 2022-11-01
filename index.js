454fcb093aea6f64a8253c6325e82014a77f212c
MEHDI BELLAM <mehdi.bellam@outlok.fr>
2022-11-01 17:16:39 +0100

    :alembic: j'essaye d'ajouter un toggle sur la navbar affin de monter notre page :alembic:

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const navLinks = document.querySelector(".nav-links");
 const social_network = document.querySelectorAll(
   "div.social-network  ul  li  "
 );
+const social_network2 = document.querySelectorAll(
+  "div.social-network  ul  li  "
+);
+
+const active_page = document.querySelectorAll(" .navbar .nav-links ul li ");
 
 menuHamburger.addEventListener("click", () => {
   navLinks.classList.toggle("mobile-menu");
@@ -11,5 +16,11 @@ menuHamburger.addEventListener("click", () => {
 menuHamburger.addEventListener("click", () => {
   social_network.forEach((social_network) => {
     social_network.classList.toggle("mobile-social-network");
+  });
+});
+
+navLinks.addEventListener("click", () => {
+  active_page.forEach((active_page) => {
+    active_page.classList.toggle("active");
   });
 });
