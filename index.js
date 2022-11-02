64d66bbbfd82d7af710acd3f0d3374f5baf34f23
MEHDI BELLAM <mehdi.bellam@outlok.fr>
2022-11-02 08:49:03 +0100

    :lipstick:  ajout d'un hoover au dessus des liens du navlinks  :lipstick:

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const navLinks = document.querySelector(".nav-links");
 const social_network = document.querySelectorAll(
   "div.social-network  ul  li  "
 );
-const social_network2 = document.querySelectorAll(
-  "div.social-network  ul  li  "
-);
 
 const active_page = document.querySelectorAll(" .navbar .nav-links ul li ");
 
@@ -16,11 +13,5 @@ menuHamburger.addEventListener("click", () => {
 menuHamburger.addEventListener("click", () => {
   social_network.forEach((social_network) => {
     social_network.classList.toggle("mobile-social-network");
-  });
-});
-
-navLinks.addEventListener("click", () => {
-  active_page.forEach((active_page) => {
-    active_page.classList.toggle("active");
   });
 });
